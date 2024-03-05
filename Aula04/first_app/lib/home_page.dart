b802fd1e4907c6992a61ed232006afcfa3bc1cbb
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-05 09:51:03 -0300

    Aula 04

---
+++ Aula04/first_app/lib/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: Drawer(),
+        appBar: AppBar(
+          title: Text('Bem-vindo!')
+        ),
+        body: Center(
+          child: Text(
+            'Hello World!',
+            textDirection: TextDirection.ltr,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.no_meals_rounded
+          ),
+          onPressed: () {
+          },
+        ),
+      );
+  }
+  
+}
---
+++ Aula04/first_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:first_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage()
+    ),
+  );
+}
+
+
